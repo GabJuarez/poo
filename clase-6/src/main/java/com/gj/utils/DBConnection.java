13b3614421def0eb71822b8c002fae37ea345624
GabJuarez <gajuarez@uamv.edu.ni>
2025-09-25 18:32:24 -0600

    aniadiendo plugin para ejecutar y poniendo la contrasenia de la conexion como variable de entorno para no subir la contrasenia a gh

--- clase-6/src/main/java/com/gj/utils/DBConnection.java
+++ clase-6/src/main/java/com/gj/utils/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class DBConnection {
     public static final String URL = "jdbc:postgresql://localhost:5432/registrosdb";
     public static final String USER = "postgres";
-    public static final String PASSWORD = "GabJuarez2025!i";
+    public static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection get() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
