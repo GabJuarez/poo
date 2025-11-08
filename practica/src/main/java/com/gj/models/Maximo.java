d1db54414169aad7301089dd7dda38b426223600
GabJuarez <gajuarez@uamv.edu.ni>
2025-11-08 14:29:07 -0600

    models

--- practica/src/main/java/com/gj/models/Maximo.java
+++ practica/src/main/java/com/gj/models/Maximo.java
@@ -1,4 +1,14 @@
 package com.gj.models;
 
 public class Maximo {
+    public static int maximo(int a, int b, int c){
+        int maximo = a;
+        if (maximo < b){
+            maximo = b;
+        }
+        if(maximo < c){
+            maximo = c;
+        }
+        return maximo;
+    }
 }
--- practica/src/main/java/com/gj/models/ProgresionGeometrica.java
+++ practica/src/main/java/com/gj/models/ProgresionGeometrica.java
@@ -1,4 +1,22 @@
 package com.gj.models;
 
+import java.util.ArrayList;
+
 public class ProgresionGeometrica {
+    public static int progresion(ArrayList<Integer> secuencia){
+        int ulidx = secuencia.size() - 1;
+        int penulidx = secuencia.size() - 2;
+        int razon = secuencia.get(ulidx) / secuencia.get(penulidx);
+        return secuencia.get(ulidx) * razon;
+
+    }
+
+    public static int alternar(int [] secuencia){
+        int ultidx = (secuencia.length-1);
+        if((ultidx + 1)% 2 == 0){
+            return secuencia[ultidx] *2;
+        }
+        return secuencia[ultidx]+2;
+
+    }
 }
