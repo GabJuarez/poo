5a510b349fd6df5a31d8f1e3cf5e4d895b83d218
GabJuarez <gajuarez@uamv.edu.ni>
2025-11-08 14:29:25 -0600

    probando models en el main

--- practica/src/main/java/com/gj/Main.java
+++ practica/src/main/java/com/gj/Main.java
@@ -1,4 +1,21 @@
 package com.gj;
 
+
+import com.gj.models.ProgresionGeometrica;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static com.gj.models.Maximo.maximo;
+import static com.gj.models.ProgresionGeometrica.progresion;
+
 public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(maximo(35,2,4));
+        var secuencia = new ArrayList<>(Arrays.asList(5, 15, 45, 135, 405));
+        System.out.println(progresion(secuencia));
+        var arr = new int[]{2, 4, 8, 10, 20, 22};
+        System.out.println(ProgresionGeometrica.alternar(arr));
+    }
 }
